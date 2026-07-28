7e345857b6efc3dbe038421b454a33fdcbde8267
maxamadibrahim2022 <maxamadibrahim2022gmail.com>
2026-07-28 10:32:41 +0300

    Exercise 1aad

---
+++ Exercise/Exercise 1/main.js
@@ -0,0 +1,31 @@
+let x = 7;
+let y = 3;
+
+
+// isku dar / isku qaybi / isku dhufo/ kala jar
+console.log(x + y );
+console.log(x / y );
+console.log(x * y );
+console.log(x - y );
+
+
+// increment
+console.log("increment");
+console.log(x++);
+console.log(x);
+
+
+// decrement
+console.log("Dicrement");
+console.log(y--);
+console.log(y);
+
+
+// pre increment
+console.log("Pre increment");
+console.log(++x);
+
+
+// Pre decrement
+console.log("Pre Decrement");
+console.log(--y);
