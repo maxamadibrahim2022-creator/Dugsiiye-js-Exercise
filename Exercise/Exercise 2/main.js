f72da2684e07e8f4de7b619ec98920adbbf217aa
maxamadibrahim2022 <maxamadibrahim2022gmail.com>
2026-07-29 10:08:27 +0300

    Exercise 2

---
+++ Exercise/Exercise 2/main.js
@@ -0,0 +1,30 @@
+// variables
+let a = 15;
+let b = "15";
+
+
+console.log("Equal or not equal to")
+// Equal to 
+console.log(a == b);
+// Not Equal to
+console.log(a != b);
+
+
+
+console.log("Sticky equal or not Sticky equal to" );
+// sticky equal to
+console.log(a === b);
+// sticky not equal to
+console.log(a !== b);
+
+
+console.log("greater than or Equal");
+// greater than or Equal
+console.log(a > b);
+console.log(a >= b);
+
+
+console.log("Less than or Equal");
+// Less than or Equal
+console.log(a < b);
+console.log(a <= b);
