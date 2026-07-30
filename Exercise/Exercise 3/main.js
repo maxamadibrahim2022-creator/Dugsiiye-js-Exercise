117075ce824a0d39f9b815bdb626bf8b401c1c86
maxamadibrahim2022 <maxamadibrahim2022gmail.com>
2026-07-30 08:11:06 +0300

    Add Exercise 3

---
+++ Exercise/Exercise 3/main.js
@@ -0,0 +1,18 @@
+let isSunny = true;
+let isWeekend = false;
+
+
+// AND &&
+console.log("AND &&");
+console.log(isSunny && isWeekend);
+
+
+//  OR ||
+console.log("OR ||");
+console.log(isSunny || isWeekend);
+
+
+// NOT !
+console.log("NOT !");
+console.log(!isSunny);
+console.log(!isWeekend);
