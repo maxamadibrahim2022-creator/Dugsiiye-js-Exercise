c28a179872baed6721cdf4521871d3622f6c5a73
maxamadibrahim2022 <maxamadibrahim2022gmail.com>
2026-08-01 09:43:13 +0300

    Add Exercise 4

---
+++ Exercise/Exercise 4/main.js
@@ -0,0 +1,23 @@
+// Bassic Function
+console.log("Bassic Function:");
+function Add(x,y){
+    console.log("Result:  $" + (x+y));
+};
+Add(10, 8);
+Add(5, 78);
+Add(13, 43);
+Add(90, 36);
+Add(100, 15);
+
+
+// Function Expression
+console.log("Function Expression:")
+const Wadar = function(x,y){
+    console.log("Wadar:  $" + (x+y));
+};
+Wadar(12,11);
+Wadar(45,98);
+Wadar(12,23);
+Wadar(23,8);
+Wadar(2,5);
+Wadar(66,4);
