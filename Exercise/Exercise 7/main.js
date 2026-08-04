a12fd52eb95d2825516006aaa3b4b809f623bb39
maxamadibrahim2022 <maxamadibrahim2022gmail.com>
2026-08-04 09:09:02 +0300

    Add Exercise 7

---
+++ Exercise/Exercise 7/main.js
@@ -0,0 +1,10 @@
+let colors = ["white", "blue", "red"]
+console.log(colors)
+
+console.log("push");
+colors.push("Gray");
+console.log(colors);
+
+console.log("shift");
+colors.shift();
+console.log(colors);
