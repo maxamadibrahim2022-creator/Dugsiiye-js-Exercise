0228b2eac5eca6c83336640640aa6e19a3357d7f
maxamadibrahim2022 <maxamadibrahim2022gmail.com>
2026-08-04 10:07:26 +0300

    Add Exercise 8

---
+++ Exercise/Exercise 8/main.js
@@ -0,0 +1,16 @@
+let car = {
+    make : 2021,
+    model : "tvs",
+    year : 2022,
+}
+
+console.log(car.make);
+console.log(car.model);
+console.log(car.year);
+
+
+//    udaabac sida array oo kale
+    console.log("udaabac sida array oo kale:  ");
+    console.log(car["make"]);
+    console.log(car["model"]);
+    console.log(car["year"]);
