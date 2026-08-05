91bfdd84c678ba9a7fba19059d950a3b2ece8cad
maxamadibrahim2022 <maxamadibrahim2022gmail.com>
2026-08-05 11:29:02 +0300

    Add Exercise 10

---
+++ Exercise/Exercise 10/main.js
@@ -0,0 +1,37 @@
+let library = [
+    {
+        Tiyle :"Maana Faay" ,
+        Auther :"Maxamad Daahir Faara" ,
+        Year  :  1993
+    },
+    
+    {
+        Tiyle : "Baarbaroosa",
+        Auther : "Jacfar Sultaan Yuusuf",
+        Year :  2022
+    },
+    {
+        Tiyle :"Sababihii Burburka Soomaaliya" ,
+        Auther :"C/qaadir Arooma" ,
+        Year :  2005
+    },
+    {
+        Tiyle :"Magaalada Jaceelka ",
+        Auther :"Ayuub Axmad Maxamuud" ,
+        Year :  2020
+    },
+    {
+        Tiyle :"Dagaalkii Raanjalis" ,
+        Auther : "Maxamaduud Cabdi Cilmi",
+        Year :  2020
+    },
+    {
+        Tiyle : "Fahamka Arimaha Sirdoonka",
+        Auther : "Daacad Mahad Cilme" ,
+        Year :  2023
+    }
+
+]
+
+console.log(library[0].Tiyle);
+console.log(library[1].Auther);
