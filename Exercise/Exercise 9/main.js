52ec9c9453ee0a5d97cd4fd9c8f7a6c13b5a3da7
maxamadibrahim2022 <maxamadibrahim2022gmail.com>
2026-08-05 09:55:34 +0300

    Add Exercise 9

---
+++ Exercise/Exercise 9/main.js
@@ -0,0 +1,13 @@
+let car = {
+    make : 2021,
+    model : "tvs",
+    year : 2022,
+    start : () => {
+        console.log("the car has started")
+    }
+}
+
+console.log(car.make);
+console.log(car.model);
+console.log(car.year);
+console.log(car.start());
