79e21a66f102e37c128f9d463e23a5d856bcd22b
maxamadibrahim2022 <maxamadibrahim2022gmail.com>
2026-08-10 07:57:17 +0300

    Add Exercise 17

---
+++ Exercise/Exercise 17/main.js
@@ -0,0 +1,12 @@
+let  Temperature = 25;
+
+
+if(Temperature < 0){
+    console.log("Very cold");
+}else if(Temperature <= 15){
+    console.log("Cold");
+}else if(Temperature <= 25){
+    console.log("Warm");
+}else{
+    console.log("Hot");
+}
