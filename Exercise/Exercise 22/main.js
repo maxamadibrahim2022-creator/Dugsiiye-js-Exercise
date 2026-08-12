8446a0c6f514c640d940fbddfe057f2f50f0c370
maxamadibrahim2022 <maxamadibrahim2022gmail.com>
2026-08-12 13:48:02 +0300

    Add Exercise 22

---
+++ Exercise/Exercise 22/main.js
@@ -0,0 +1,10 @@
+                                    // **Exercise:**
+// 1. Use array destructuring to extract the first and third elements from the array `["red", "green", "blue"]`.
+
+
+
+                    // solution
+const colors = ["Red", "Green", "Blue"];
+let [Red, Green, Blue] = colors;
+console.log(Red);
+console.log(Blue );
