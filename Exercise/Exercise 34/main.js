724115e3e25d5983b8abbbcf10405a885edf01dc
maxamadibrahim2022 <maxamadibrahim2022gmail.com>
2026-08-20 10:55:39 +0300

    Add Exercise 34

--- Exercise/Exercise 34/main.js
+++ Exercise/Exercise 34/main.js
@@ -1,3 +1,9 @@
+// Exercise 4: Adding and Removing Elements
+// Task: Create two buttons—one to add a new list item to a list and another to remove the last item from the list. Implement the `addItem` and `removeItem` functions as shown in the example.
+
+
+
+// Solution
 const list = document.querySelector('#list')
 function addList(){
     const newList = document.createElement('li')
